2c93d9e81e7df251d17f5255b4d3c21bd9ba8168
Michał Bielicki <mbielicki2912@gmail.com>
2024-10-18 10:39:26 +0200

    add gmean noise filter

--- img_operations/noise_removal.py
+++ img_operations/noise_removal.py
@@ -1,25 +1,26 @@
 import numpy as np
+from scipy.stats import gmean
 from cli.allowed_args import assert_only_allowed_args
 
 
-def median(args, arr):
+def get_box_values(arr, x, y, c, box_width, box_height, img_width, img_height) -> list[int]:
+    box = []
+    for s in range(x - box_width // 2, x + box_width // 2 + 1):
+        for t in range(y - box_height // 2, y + box_height // 2 + 1):
+            if s >= 0 and s < img_width and t >= 0 and t < img_height:
+                box.append(arr[t, s, c])
+
+    return box
+
+def median_filter(args, arr):
     assert_only_allowed_args(args, [])
     
     height = arr.shape[0]
     width = arr.shape[1]
     colors = arr.shape[2]
 
-    box_width = 4
-    box_height = 4
-
-    def box_values(x, y, c):
-        box = []
-        for s in range(x - box_width // 2, x + box_width // 2 + 1):
-            for t in range(y - box_height // 2, y + box_height // 2 + 1):
-                if s >= 0 and s < width and t >= 0 and t < height:
-                    box.append(arr[t, s, c])
-
-        return box
+    BOX_WIDTH = 3
+    BOX_HEIGHT = 3
 
     new_arr = np.zeros((height, width, colors))
 
@@ -27,10 +28,28 @@ def median(args, arr):
         for x in range(width):
             for c in range(colors):
 
-                box = box_values(x, y, c)
+                box = get_box_values(arr, x, y, c, BOX_WIDTH, BOX_HEIGHT, width, height)
                 new_arr[y, x, c] = np.median(box)
 
     return new_arr
 
-def gmean():
-    pass
+def gmean_filter(args, arr):
+    assert_only_allowed_args(args, [])
+    
+    height = arr.shape[0]
+    width = arr.shape[1]
+    colors = arr.shape[2]
+
+    BOX_WIDTH = 3
+    BOX_HEIGHT = 3
+
+    new_arr = np.zeros((height, width, colors))
+
+    for y in range(height):
+        for x in range(width):
+            for c in range(colors):
+
+                box = get_box_values(arr, x, y, c, BOX_WIDTH, BOX_HEIGHT, width, height)
+                new_arr[y, x, c] = gmean(box)
+
+    return new_arr
