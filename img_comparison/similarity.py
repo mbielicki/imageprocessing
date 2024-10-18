618771b3bc57e85787dbe0f3a4166e6b51d43d10
Michał Bielicki <mbielicki2912@gmail.com>
2024-10-18 18:55:24 +0200

    add snr similarity

--- img_comparison/similarity.py
+++ img_comparison/similarity.py
@@ -29,7 +29,17 @@ def pmse(args: dict, input_im: np.ndarray, ref_im: np.ndarray) -> str:
     return f"Peak Mean Square Error: {pmse:.4f}"
 
 def snr(args: dict, input_im: np.ndarray, ref_im: np.ndarray) -> str:
-    pass
+    assert_only_allowed_args(args, ['--input', '--reference']) 
+
+    input_im = input_im.astype(np.float64)
+    ref_im = ref_im.astype(np.float64)
+
+    mean_squares = (ref_im ** 2).mean()
+    mse = ((ref_im - input_im) ** 2).mean()
+    
+    snr = 10 * np.log10(mean_squares / mse)
+    
+    return f"Signal to noise ratio: {snr:.2f} dB"
 
 def psnr(args: dict, input_im: np.ndarray, ref_im: np.ndarray) -> str:
     pass
