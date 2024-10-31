76b196a5815b449d2c9a4e7cb7440da5c9dd189d
Michał Bielicki <mbielicki2912@gmail.com>
2024-10-31 18:38:50 +0100

    add mean() characteristic

--- img_analysis/characteristics.py
+++ img_analysis/characteristics.py
@@ -1,2 +1,18 @@
-def mean(args, arr):
-    return "Mean is not implemented yet"
+import numpy as np
+from constants import PIXEL_VALUE_RANGE
+from histogram import get_histogram
+
+
+def mean(args: dict, arr: np.ndarray) -> str:
+    width = arr.shape[1]
+    height = arr.shape[0]
+    colors = arr.shape[2]
+
+    N = width * height
+    L = PIXEL_VALUE_RANGE
+    
+    hist = get_histogram(arr)
+
+    mean = np.multiply(hist, np.arange(L), out=hist).sum() / N
+
+    return f"Mean: {mean:.2f}"
