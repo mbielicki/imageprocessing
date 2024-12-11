02ae5092049fb4c136fc36db37f0a9c8b0b6f3be
Michał Bielicki <mbielicki2912@gmail.com>
2024-12-11 18:02:27 +0100

    mean similarity - bad

--- img_transformations/region_growing.py
+++ img_transformations/region_growing.py
@@ -7,10 +7,68 @@ from utils import time_it
 import matplotlib.pyplot as plt
 
 class Region:
-    def __init__(self, seed: tuple, color: tuple):
+    def __init__(self, seed: tuple, color: tuple, arr: np.ndarray, map: np.ndarray, threshold: int):
         self.seed = seed
         self.color = color
-        self.filled = False
+        self.done = False
+        self.mean = arr[seed]
+        self.size = 1
+
+        self.map = map
+        self.arr = arr
+        self.regions = []
+
+        self.threshold = threshold
+
+        self.queue = [seed]
+    
+    def add_to_mean(self, v: np.ndarray):
+        self.size += 1
+        self.mean = self.mean + v / (self.size)
+    
+    def grow(self):
+        next_queue = []
+        while self.queue:
+            p = self.queue.pop(0)
+            if self.is_similar(self.arr[p]):
+                if self._is_0(p):
+                    self.add_to_mean(self.arr[p])
+                    self.map[p] = self.color
+                    for pi in get_neighbors(p, self.map.shape):
+                        next_queue.append(pi)
+                elif self._is_already_in(p):
+                    continue
+                else: # is in other region
+                    r = self._get_region_whose_seed_is(p)
+                    if r is not None:
+                        self.merge(r)
+
+        if not next_queue:
+            self.done = True
+        self.queue = next_queue
+        return self.map
+    
+    def is_similar(self, val) -> bool:
+        val = np.array(val)
+        if np.all(val - self.threshold < self.mean) and np.all(self.mean < val + self.threshold):
+            return True
+
+    def _is_0(self, p):
+        return np.all(self.map[p] == 0)
+    
+    def _is_already_in(self, p):
+        return np.all(self.map[p] == self.color)
+    
+    def _get_region_whose_seed_is(self, p):
+        for r in self.regions:
+            if r.seed == p:
+                return r
+            
+    def merge(self, r: 'Region'):
+        self.mean = (self.mean * self.size + r.mean * r.size) / (self.size + r.size)
+        self.size += r.size
+        r.done = True
+        return np.where(self.map == r.color, self.color, self.map)
 
 @time_it
 def region_growing(args: dict, arr: np.ndarray) -> np.ndarray:
@@ -19,38 +77,27 @@ def region_growing(args: dict, arr: np.ndarray) -> np.ndarray:
     height, width, colors = arr.shape
     seed_n = get_int_arg(args, '--seeds', default=2, range=(1, width * height))
 
-    regions = choose_seeds(arr, seed_n)
     map = np.zeros((height, width, 3), dtype=np.uint8)
+    regions = choose_seeds(arr, seed_n, map, threshold)
 
     for region in regions:
-        if region.filled:
-            continue
-        similar = get_similar(region.seed, arr, threshold)
-        map = join_adjacent(map, similar, region, regions)
+        region.regions = regions
+        while not region.done:
+            map = region.grow()
 
-    return map
 
-def join_adjacent(map: np.ndarray, similar: np.ndarray, region: Region, regions: list[Region]):
-    seed = region.seed
-    stack = [seed]
-    while stack:
-        p = stack.pop()
-        map[p] = region.color
-        for pi in get_neighbors(p, map.shape[0:2]):
-            if similar[pi]:
-                if np.all(map[pi] == [0, 0, 0]): # if pi is seed, merge regions
-                    stack.append(pi)
-                else:
-                    for r in regions:
-                        if r.seed == pi:
-                            r.color = region.color
-                            r.filled = True
-    region.filled = True
+    # while regions:
+    #     region = regions.pop(0)
+    #     map = region.grow()
+    #     if not region.done:
+    #         map = region.grow()
+    #         regions.append(region)
+
     return map
 
 def get_neighbors(p, shape) -> np.ndarray:
     y, x = p
-    height, width = shape
+    height, width, _ = shape
     potential_neighbors = [(y, x+1), 
                  (y, x-1),
                  (y+1, x),
@@ -64,15 +111,15 @@ def get_neighbors(p, shape) -> np.ndarray:
     return res
     
     
-def get_similar(s, arr, threshold):
-    return np.all(((arr - threshold < arr[s]) & (arr[s] < arr + threshold)), axis=2)
 
-def choose_seeds(arr: np.ndarray, seed_n: int) -> list[Region]:
+
+def choose_seeds(arr: np.ndarray, seed_n: int, map, threshold) -> list[Region]:
     height, width, colors = arr.shape
     ns = np.random.randint(0, height * width, seed_n)
     xs = ns % width
     ys = ns // width
-    return [Region((ys[i], xs[i]), get_color(i)) for i in range(seed_n)]
+    return [Region((ys[i], xs[i]), get_color(i), arr, map, threshold) 
+            for i in range(seed_n)]
 
 colors = np.array([
         [158, 1, 66],
